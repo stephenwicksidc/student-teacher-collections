0ecc465272e4b60ed872d00713f190662ad77a7d
Stephen Wicks <stevewicks1@gmail.com>
2017-07-26 15:59:51 -0400

    first commit

---
+++ src/swidc/Main.java
@@ -0,0 +1,13 @@
+package swidc;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList<String> firstGradeStudents = new ArrayList<>();
+        firstGradeStudents.add(0,"Stephen");
+        System.out.println(firstGradeStudents);
+    }
+}
---
+++ src/swidc/Student.java
@@ -0,0 +1,49 @@
+package swidc;
+
+/**
+ * Created by stephenwicks on 7/26/17.
+ */
+public class Student {
+        String firstName;
+        String lastName;
+        int id;
+        int grade;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public Student() {
+
+    }
+
+
+}
